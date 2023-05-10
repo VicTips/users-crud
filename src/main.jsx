84e78bcaab7dbcba3d145f04b5a8b2618b83c186
victips <torvicarias@gmail.com>
2023-05-10 14:19:04 -0500

    Remove redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./router/index.jsx";
 import { RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
